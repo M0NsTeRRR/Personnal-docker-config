205ad58605a06e6739c4e4462f07ff3586d18b6d
Ludovic Ortega <mastership@hotmail.fr>
2019-06-17 11:21:22 +0200

    Fix
    Downgrade docker-compose log version
    Add restart directive

--- start.sh
+++ start.sh
@@ -3,9 +3,24 @@ docker-compose \
 --project-directory . \
 -f reverse-proxy/docker-compose.yml \
 -f monitoring/docker-compose.yml \
--f log/docker-compose.yml \
 -f personnal-website/docker-compose.yml \
 -f wiki/docker-compose.yml \
 -f git/docker-compose.yml \
+up \
+-d
+
+cd mail-server/mailcow-dockerized/
+docker-compose \
+-p personnal-docker-config \
+--project-directory ../.. \
+-f docker-compose.yml \
+up \
+-d
+cd ../..
+
+docker-compose \
+-p personnal-docker-config \
+--project-directory . \
+-f log/docker-compose.yml \
 up \
 -d
